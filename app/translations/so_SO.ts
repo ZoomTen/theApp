527d72920b96a2c1112cdc68c8c36297a2b859e9
ZoomTen <daxuya.zumi+code@protonmail.com>
2020-06-17 17:49:31 +0700

    translation spam

---
+++ app/translations/so_SO.ts
@@ -0,0 +1,181 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="so_SO" sourcelanguage="en_US">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../mainwindow.cpp" line="78"/>
+        <source>File</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="87"/>
+        <source>Help</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="104"/>
+        <source>Hello World!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="105"/>
+        <source>This is a sample custom third-party app that uses theSuite library. This page is the simplest widget here.
+
+&quot;Where&apos;s the menu?&quot; I hear you say. &quot;Why, click on the question mark logo at the top,&quot; I reply.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="108"/>
+        <source>Widget Gallery</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="109"/>
+        <source>A testing playground, for all your testing needs.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Page1</name>
+    <message>
+        <location filename="../page1/page1.ui" line="14"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../page1/page1.ui" line="28"/>
+        <source>Header</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../page1/page1.ui" line="53"/>
+        <source>Contents Contents Contents Contents Contents Contents Contents Contents Contents Contents Contents Contents Contents Contents </source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Page2</name>
+    <message>
+        <location filename="../page2/page2.ui" line="14"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../page2/page2.ui" line="28"/>
+        <source>Header</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../page2/page2.ui" line="53"/>
+        <source>Contents Contents Contents Contents Contents Contents Contents Contents Contents Contents Contents Contents Contents Contents </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../page2/page2.ui" line="83"/>
+        <source>Bell</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../page2/page2.ui" line="96"/>
+        <source>Slide up</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../page2/page2.ui" line="103"/>
+        <source>Notification</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../page2/page2.ui" line="110"/>
+        <source>Flash this button</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../page2/page2.ui" line="123"/>
+        <source>Horizontal slide</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../page2/page2.ui" line="136"/>
+        <source>Popover</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../page2/page2.ui" line="149"/>
+        <source>Toast</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../page2/page2.ui" line="156"/>
+        <source>Message</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../page2/page2.ui" line="169"/>
+        <source>Fade</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../page2/page2.ui" line="176"/>
+        <source>Page transition type</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../page2/page2.cpp" line="91"/>
+        <source>You know what they say...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../page2/page2.cpp" line="92"/>
+        <source>All toasters toast toast :p</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../page2/page2.cpp" line="120"/>
+        <source>This is a notification</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../page2/page2.cpp" line="121"/>
+        <source>May I have your attention, please?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../page2/page2.cpp" line="125"/>
+        <source>I&apos;m awake</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../page2/page2.cpp" line="148"/>
+        <source>I&apos;m a robot</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../page2/page2.cpp" line="149"/>
+        <source>Beep boop</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Page2Popover</name>
+    <message>
+        <location filename="../page2/popover/page2_popover.ui" line="35"/>
+        <source>Hey!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../page2/popover/page2_popover.ui" line="45"/>
+        <source>You can close this popover by clicking on the &quot;back&quot; button or by clicking outside this box.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../main.cpp" line="70"/>
+        <source>Basic application</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
